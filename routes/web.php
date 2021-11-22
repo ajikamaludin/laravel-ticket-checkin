a82b03e953ace27e978681643c06b48f10602d12
Aji Kamaludin <aji19kamaludin@gmail.com>
2021-11-22 08:47:09 +0700

    grouping route

--- routes/web.php
+++ routes/web.php
@@ -25,21 +25,25 @@ Route::post('/', [FrontPageController::class, 'store'])->name('frontpage.store')
 Route::get('/login', [AuthController::class, 'formLogin'])->name('login')->middleware('guest');
 Route::post('/login', [AuthController::class, 'login'])->name('login.store')->middleware('guest');
 
-// Route::get('/logout', [AuthController::class, 'logout'])->name('auth.logout');
-Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
-Route::get('/dashboard', [DashboardContoller::class, 'index'])->name('dashboard')->middleware('auth');
 
-//
-Route::get('/tickets/check', [TicketController::class, 'search'])->name('tickets.search');
-Route::post('/tickets/check', [TicketController::class, 'check'])->name('tickets.check');
+Route::middleware(['auth'])->group(function () {
+    // Route::get('/logout', [AuthController::class, 'logout'])->name('auth.logout');
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
-// resources
-// Route::get('/tickets', [TicketController::class, 'index'])->name('tickets.index')->middleware('auth');
-// Route::get('/tickets/create', [TicketController::class, 'create'])->name('tickets.create')->middleware('auth');
-// Route::post('/tickets', [TicketController::class, 'store'])->name('tickets.store')->middleware('auth');
-// Route::get('/tickets/{ticket}', [TicketController::class, 'edit'])->name('tickets.edit')->middleware('auth');
-// Route::put('/tickets/{ticket}', [TicketController::class, 'update'])->name('tickets.update')->middleware('auth');
-// Route::delete('/tickets/{ticket}', [TicketController::class, 'destroy'])->name('tickets.destroy')->middleware('auth');
+    Route::get('/dashboard', [DashboardContoller::class, 'index'])->name('dashboard');
 
-Route::resource('tickets', TicketController::class);
+    //
+    Route::get('/tickets/check', [TicketController::class, 'search'])->name('tickets.search');
+    Route::post('/tickets/check', [TicketController::class, 'check'])->name('tickets.check');
+
+    // resources
+    // Route::get('/tickets', [TicketController::class, 'index'])->name('tickets.index');
+    // Route::get('/tickets/create', [TicketController::class, 'create'])->name('tickets.create');
+    // Route::post('/tickets', [TicketController::class, 'store'])->name('tickets.store');
+    // Route::get('/tickets/{ticket}', [TicketController::class, 'edit'])->name('tickets.edit');
+    // Route::put('/tickets/{ticket}', [TicketController::class, 'update'])->name('tickets.update');
+    // Route::delete('/tickets/{ticket}', [TicketController::class, 'destroy'])->name('tickets.destroy');
+
+    Route::resource('tickets', TicketController::class);
+});
